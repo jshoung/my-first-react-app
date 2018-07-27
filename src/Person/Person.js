87c519f41dae0c214128ea7e846630b6aa4eeb92
jshoung <julianks@gmail.com>
2018-07-27 18:06:49 +0800

    Add delete person button

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,10 +4,11 @@ import './Person.css';
 
 const person = (props) => {
   return (
-    <div className='Person' onClick={props.click}>
+    <div className='Person'>
       <p>I'm {props.name} and I am {props.age} years old!</p>
       <p>My Hobbies: {props.hobbies}</p>
       <input type='text' onChange={props.changed} value={props.name} />
+      <button onClick={props.click}>Delete Person</button>
     </div>
   )
 }
